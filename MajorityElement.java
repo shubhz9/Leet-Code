ff2e5443c21fca7e9fb3a7347455bae31bb83a21
shubhz9 <shubhamrajendraraut@gmail.com>
2023-09-15 15:38:13 +0530

    169. Majority Element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,3 +1,20 @@
+/* 
+169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+LINK - https://leetcode.com/problems/majority-element/description/
+*/
+
 class MajorityElement {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> occurences = new HashMap<Integer, Integer>();
